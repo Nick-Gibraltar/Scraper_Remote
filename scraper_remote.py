4e330c2d5a288e9b0594221a64e2a3b3390662d2
Nick <nicholas.gibbins@blueyonder.co.uk>
2023-01-11 13:08:24 +0000

    Adds new option

--- scraper_remote.py
+++ scraper_remote.py
@@ -36,7 +36,8 @@ class Scraper:
         chrome_options.add_argument("--disable-setuid-sandbox")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("window-size=1920,1080")
-        
+        chrome_options.add_argument("user-agent='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.5005 Safari/537.36'") 
+
 
         print("3. Scraper __init__")
         self.url = "https://www.screwfix.com"
